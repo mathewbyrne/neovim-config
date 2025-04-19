3c80f557fdf243f6f84e4d15265e93b4d533cc31
Mathew Byrne <mathewbyrne@gmail.com>
2025-04-19 23:19:13 +1000

    changes

--- lua/bitmat/options.lua
+++ lua/bitmat/options.lua
@@ -1,3 +1,4 @@
+
 vim.opt.number = true
 vim.opt.relativenumber = true
 
@@ -6,3 +7,10 @@ vim.opt.shiftwidth = 4
 vim.opt.tabstop = 4
 vim.opt.softtabstop= 4
 
+vim.opt.smartindent = true
+
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+
+
+vim.opt.colorcolumn = "80"
