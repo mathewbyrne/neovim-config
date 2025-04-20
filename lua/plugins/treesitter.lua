5a1d1a5005d97555f8b9f4b234f2609bb63c23ef
Mathew Byrne <mathewbyrne@gmail.com>
2025-04-20 11:23:27 +1000

    add treesitter textobjects config
    started with allowing jumping between function definintions

---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,31 @@
+return {
+    "nvim-treesitter/nvim-treesitter",
+    dependencies = {
+        "nvim-treesitter/nvim-treesitter-textobjects",
+    },
+    build = ":TSUpdate",
+    config = function ()
+        local configs = require("nvim-treesitter.configs")
+
+        configs.setup({
+            ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "elixir", "heex", "javascript", "html" },
+            sync_install = false,
+            highlight = { enable = true },
+            indent = { enable = true },
+            textobjects = {
+                move = {
+                    enable = true,
+                    set_jumps = true,
+                    goto_next_start = {
+                        ["]]"] = "@function.outer",
+                    },
+                    goto_previous_start = {
+                        ["[["] = "@function.outer",
+                    },
+                }
+            }    
+        })
+
+        
+    end
+}
