a1a6f84ff605d2c69126b95fc5376a0bb3682cb9
Mathew Byrne <mathewbyrne@gmail.com>
2026-02-17 18:55:14 +1100

    update config command tab name

--- lua/bitmat/init.lua
+++ lua/bitmat/init.lua
@@ -3,6 +3,10 @@ vim.api.nvim_create_user_command('BitmatNvimConfig', function()
     vim.cmd('tabnew')
     vim.cmd('tcd ' .. vim.fn.stdpath('config'))
     vim.cmd('Ex')
+
+    if vim.fn.exists(':Tabby') > 0 then
+        vim.cmd('Tabby rename_tab Config')
+    end
 end, {})
 
 require('bitmat.remap')
